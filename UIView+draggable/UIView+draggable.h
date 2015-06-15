6dcfe86733e9a5e2d28bce5e93d4dde5bfb5a40b
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-06-15 21:23:31 +0200

    Add carthage support

---
+++ UIView+draggable/UIView+draggable.h
@@ -0,0 +1,18 @@
+//
+//  UIView+draggable.h
+//  UIView+draggable
+//
+//  Created by Andrea Mazzini on 15/06/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIView+draggable.
+FOUNDATION_EXPORT double UIView_draggableVersionNumber;
+
+//! Project version string for UIView+draggable.
+FOUNDATION_EXPORT const unsigned char UIView_draggableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIView_draggable/PublicHeader.h>
+#import <UIView_draggable/UIView+draggable.h>
