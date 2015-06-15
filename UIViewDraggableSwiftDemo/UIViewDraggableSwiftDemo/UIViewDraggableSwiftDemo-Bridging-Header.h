4194d222f0c50c3c1c67a3a1af760908c5f312c0
viccarre <viccarre@gmail.com>
2015-06-15 03:42:34 -0400

    Swift Demo

---
+++ UIViewDraggableSwiftDemo/UIViewDraggableSwiftDemo/UIViewDraggableSwiftDemo-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  UIViewDraggableSwiftDemo-Bridging-Header.h
+//  UIViewDraggableSwiftDemo
+//
+//  Created by Victor Carreño on 6/15/15.
+//  Copyright (c) 2015 Victor Carreño. All rights reserved.
+//
+
