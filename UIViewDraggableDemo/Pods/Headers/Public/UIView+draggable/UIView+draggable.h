49a4348456a0ca1ceee69e6fb5606fa15fde2f48
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-03-31 10:43:23 +0200

    Update demo project

---
+++ UIViewDraggableDemo/Pods/Headers/Public/UIView+draggable/UIView+draggable.h
@@ -0,0 +1,1 @@
+../../../../../Source/UIView+draggable.h
--- UIViewDraggableDemo/Pods/Target Support Files/Pods-UIViewDraggableDemo/Pods-UIViewDraggableDemo-frameworks.sh
+++ UIViewDraggableDemo/Pods/Target Support Files/Pods-UIViewDraggableDemo/Pods-UIViewDraggableDemo-frameworks.sh
@@ -82,10 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-UIViewDraggableDemo/UIView_draggable.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-UIViewDraggableDemo/UIView_draggable.framework"
-fi
